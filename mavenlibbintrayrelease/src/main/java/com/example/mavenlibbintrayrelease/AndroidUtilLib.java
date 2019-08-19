b79bb666a162ec63077b1cce980fa6f1c2d87f7c
xuqi <xuqi@changba.com>
2019-08-19 17:08:48 +0800

    add android lib

---
+++ mavenlibbintrayrelease/src/main/java/com/example/mavenlibbintrayrelease/AndroidUtilLib.java
@@ -0,0 +1,10 @@
+package com.example.mavenlibbintrayrelease;
+
+/**
+ * Created by xuqi on 2019-08-19
+ */
+public class AndroidUtilLib {
+    public static String getString() {
+        return "AndroidUtilLib getString success";
+    }
+}
