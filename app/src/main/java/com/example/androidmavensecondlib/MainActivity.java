8944e6b5d02a0d5a67fda962f4929a50768a523c
xuqi <xuqi@changba.com>
2019-08-20 10:50:50 +0800

    添加对Android bintray lib的引用

--- app/src/main/java/com/example/androidmavensecondlib/MainActivity.java
+++ app/src/main/java/com/example/androidmavensecondlib/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.androidmavensecondlib;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.mavenlibbintrayrelease.AndroidUtilLib;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "AndroidUtilLib.getString() = " + AndroidUtilLib.getString());
+        Log.d(TAG, "AndroidUtilLib.getString() version = " + AndroidUtilLib.getStringVersion());
     }
 }
